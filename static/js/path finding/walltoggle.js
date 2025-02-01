b07e35ade09dbf0036d1a391d63ced298611abc6
Utkarsh Jaiswal <utalmighty@gmail.com>
2025-02-01 10:02:25 +0000

    🔧 Fixed Dragging

--- static/js/path finding/walltoggle.js
+++ static/js/path finding/walltoggle.js
@@ -1,7 +1,22 @@
 // Controls the cell color on click.
+let mouseButtonDown = false;
+
+document.body.addEventListener('mousedown', (e) => {    
+    mouseButtonDown = true;
+});
+
+document.body.addEventListener('mouseup', (e) => {
+    mouseButtonDown = false;
+})
+
 function walltoggle() {
     for (var i = 0; i < document.querySelectorAll("td").length; i++) {
-         document.querySelectorAll("td")[i].addEventListener("mouseover", function () { this.classList.toggle("wall"); });
+        document.querySelectorAll("td")[i].addEventListener('mouseover', function() {
+            if (mouseButtonDown) {
+                this.classList.toggle("wall");
+            }
+        });
     }
 }
+
 // BY UTKARSH JAISWAL
